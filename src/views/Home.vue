9e8a79ab75f105b220369568e00427677b11c4aa
ayyHA <ayyHaibaraAi@gmail.com>
2022-04-16 23:40:18 +0800

    update stomp

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -19,15 +19,64 @@
 import Menu from "@/components/menu.vue";
 import News from "@/components/news.vue";
 import MenuTitle from "@/components/menuTitle.vue";
+import Stomp from "stompjs";
 
 export default {
   data() {
-    return {};
+    return {
+      client: null,
+    };
   },
   components: {
     Menu,
     News,
     MenuTitle,
+  },
+  created() {
+    // 初始化连接
+    this.connect();
+  },
+  methods: {
+    // 点赞通知
+    likeNotification(msg) {
+      this.$notify({
+        title: "点赞消息",
+        message: msg,
+        // duration: 1,
+      });
+    },
+    // 连接成功的回调函数
+    onConnected(frame) {
+      console.log("Connected: " + frame);
+      // 绑定exchange和routingKey
+      let exchange = "/exchange/like_exchange/" + this.$store.state.userInfo.id;
+      // 订阅对应的交换机,通过路由键找到对应的队列,后面是接收消息和失败的回调函数
+      this.client.subscribe(exchange, this.onReceived, this.onFailed);
+    },
+    // 连接失败的回调函数
+    onFailed(frame) {
+      console.log("Failed: " + frame);
+      // 重连
+      window.setTimeout(() => {
+        this.connect();
+      }, 5000);
+    },
+    // 收到消息后的回调函数
+    onReceived(frame) {
+      console.log("收到的消息 msg=>" + frame.body);
+      this.likeNotification(frame.body);
+    },
+    // 连接RabbitMQ
+    connect() {
+      this.client = Stomp.client("ws://localhost:15674/ws");
+      let headers = {
+        login: "ayyHA",
+        passcode: "123456aa",
+        host: "/",
+      };
+      // 创建连接，放入连接成功和失败的回调函数
+      this.client.connect(headers, this.onConnected, this.onFailed);
+    },
   },
 };
 </script>
